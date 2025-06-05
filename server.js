7108ee74538fd332684a47a598b6056c899571d9
Austin Brooksby <brooksby.austin@gmail.com>
2025-06-05 12:56:22 -0700

    Testing 4o-mini

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/chat', async (req, res) => {
     const messages = req.body.messages;
 
     const chatResponse = await openai.chat.completions.create({
-      model: 'gpt-4.1-nano',
+      model: 'gpt-4o-mini',
       messages: messages,
     });
 
